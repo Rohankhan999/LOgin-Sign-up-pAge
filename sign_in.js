65507f93cb7ac94787fb9f09886f24302e99f046
Rohankhan999 <rohankhan08453@gmail.com>
2024-12-10 19:40:28 +0500

    Update sign_in.js

--- sign_in.js
+++ sign_in.js
@@ -1,16 +1,24 @@
 function Signin() {
     var Username = document.querySelector('#UserName');
     var Password =document.querySelector('#Password')
-
+    if(Username.value === "" || Password.value === ""){
+      Swal.fire({
+        icon: "error",
+        title: "ERROR...",
+        text: "Enter Password or UserName!",
+    });
+    return
+  }
     localStorage.setItem('Username',Username.value)
     localStorage.setItem('Password',Password.value)
 
     Username.value =""
     Password.value =""
+   
     Swal.fire({
         title: "Sign _up SuccessFully!",
         text: "Welcome Here!!",
         icon: "success"
       });
-
+    window.open("login_page.html","_self")
 }
